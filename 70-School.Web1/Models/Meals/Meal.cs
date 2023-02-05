973bee720ab61e3d7c486e39e8d838e8f57ab676
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-02-05 12:53:07 +0300

    DATA: Model and Migrate Meals

---
+++ 70-School.Web1/Models/Meals/Meal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _70_School.Web1.Models.Meals
+{
+    public class Meal
+    {
+        public Guid Id { get; set; }
+        public MealType Type { get; set; }
+        public double Price { get; set; }
+        public Guid StudentId { get; set; }
+        public MealStatus Status { get; set; }
+        public int Quantity { get; set; }
+        public Guid CreateBy { get; set; }
+        public Guid UpdateBy { get; set; }
+        public DateTimeOffset CreateDate { get; set; }
+        public DateTimeOffset UpdateDate { get; set;}
+    }
+}
