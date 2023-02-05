cd615c459fdf972595e1fb43080b60ef09f5d2fc
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-02-05 07:56:55 +0300

    INFRA: Initialize Build Project

---
+++ 70-School.Infrastructure.Build/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+using System.Collections.Generic;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "70-School Build Process",
+
+    OnEvents = new Events
+    {
+        Push = new PushEvent
+        {
+            Branches = new string[] { "master" }
+        },
+
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "master" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2022,
+
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Check Out"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Setup .Net",
+
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "7.0.100",
+                        IncludePrerelease = true,
+                    }
+                },
+
+
+                new RestoreTask
+                {
+                    Name = "Restore Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Build Projects"
+                },
+
+                new TestTask
+                {
+                    Name = "Run Test"
+                }
+            }
+        }
+    }
+};
+
+var adotnetClient = new ADotNetClient();
+
+adotnetClient.SerializeAndWriteToFile(
+    adoPipeline: githubPipeline,
+    path: "../../../../.github/workflows/build.yml");
