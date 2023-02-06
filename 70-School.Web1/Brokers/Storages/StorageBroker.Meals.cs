54b2bba1b71874efa38e0619388d2fbf9a22b60f
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-02-06 20:48:37 +0300

    BROKERS: Insert Meals

---
+++ 70-School.Web1/Brokers/Storages/StorageBroker.Meals.cs
@@ -0,0 +1,11 @@
+﻿using _70_School.Web1.Models.Meals;
+using System.Threading.Tasks;
+
+namespace _70_School.Web1.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public async ValueTask<Meal> InsertMealAsync(Meal meal) =>
+            await InsertAsync(meal);
+    }
+}
