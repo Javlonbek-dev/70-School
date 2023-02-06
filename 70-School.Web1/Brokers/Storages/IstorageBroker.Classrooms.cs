10590ba4ec64a337672925d083e9b422e16e3ab2
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-02-06 08:23:07 +0300

    BROKERS: Select Classroom By Id

--- 70-School.Web1/Brokers/Storages/IstorageBroker.Classrooms.cs
+++ 70-School.Web1/Brokers/Storages/IstorageBroker.Classrooms.cs
@@ -1,4 +1,5 @@
 ﻿using _70_School.Web1.Models.Classrooms;
+using System;
 using System.Threading.Tasks;
 
 namespace _70_School.Web1.Brokers.Storages
@@ -6,5 +7,6 @@ namespace _70_School.Web1.Brokers.Storages
     public partial interface IstorageBroker
     {
         ValueTask<Classroom> InsertClassroom(Classroom classroom);
+        ValueTask<Classroom> SelectClasroomById(Guid Id);
     }
 }
--- 70-School.Web1/Brokers/Storages/StorageBroker.Classrooms.cs
+++ 70-School.Web1/Brokers/Storages/StorageBroker.Classrooms.cs
@@ -1,11 +1,15 @@
 ﻿using _70_School.Web1.Models.Classrooms;
+using System;
 using System.Threading.Tasks;
 
 namespace _70_School.Web1.Brokers.Storages
 {
     public partial class StorageBroker
     {
-        public async ValueTask<Classroom> InsertClassroom(Classroom classroom)=>
+        public async ValueTask<Classroom> InsertClassroom(Classroom classroom) =>
             await InsertAsync(classroom);
+
+        public async ValueTask<Classroom> SelectClasroomById(Guid Id) =>
+            await SelectAsync<Classroom>(Id);
     }
 }
