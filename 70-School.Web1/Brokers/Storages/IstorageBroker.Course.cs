8244875b3eb113dec3d2e3ce06e1f77d64abef13
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-02-13 12:11:03 +0300

    BROKERS: Inseert Course

---
+++ 70-School.Web1/Brokers/Storages/IstorageBroker.Course.cs
@@ -0,0 +1,10 @@
+﻿using _70_School.Web1.Models.Course;
+using System.Threading.Tasks;
+
+namespace _70_School.Web1.Brokers.Storages
+{
+    public partial interface IstorageBroker
+    {
+        ValueTask<Course> InsertCourseAsync(Course course);
+    }
+}
---
+++ 70-School.Web1/Brokers/Storages/StorageBroker.Course.cs
@@ -0,0 +1,11 @@
+﻿using _70_School.Web1.Models.Course;
+using System.Threading.Tasks;
+
+namespace _70_School.Web1.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public async ValueTask<Course> InsertCourseAsync(Course course)=>
+            await InsertAsync(course);
+    }
+}
