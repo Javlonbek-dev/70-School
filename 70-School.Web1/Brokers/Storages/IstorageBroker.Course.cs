70e901d03f58c607dabbe2834892db0fcbfbc7c9
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-02-13 12:14:32 +0300

    BROKERS: Select All Coureses

--- 70-School.Web1/Brokers/Storages/IstorageBroker.Course.cs
+++ 70-School.Web1/Brokers/Storages/IstorageBroker.Course.cs
@@ -1,4 +1,5 @@
 ﻿using _70_School.Web1.Models.Course;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace _70_School.Web1.Brokers.Storages
@@ -6,5 +7,6 @@ namespace _70_School.Web1.Brokers.Storages
     public partial interface IstorageBroker
     {
         ValueTask<Course> InsertCourseAsync(Course course);
+        IQueryable<Course> SelectAllCourses();
     }
 }
