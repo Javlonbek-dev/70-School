1a931e50b9a72a39137ec36fdd5db0b14d330298
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-02-13 12:37:37 +0300

    BROKERS : Add Logging

---
+++ 70-School.Web1/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _70_School.Web1.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
