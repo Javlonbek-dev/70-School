799fa0d406d47696fd039e8943d75d81992b0caa
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-03-18 13:12:30 +0300

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ 70-School.Tests.Unit/Foundations/Students/StudnetsServiceTests.Validation.RetieveById.cs
@@ -0,0 +1,46 @@
+﻿using _70_School.Web1.Models.Students;
+using _70_School.Web1.Models.Students.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace _70_School.Tests.Unit.Foundations.Students
+{
+    public partial class StudnetsServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            //given
+            var invalidStudentId = Guid.Empty;
+            var invalidStudentException = new InvalidStudentException();
+
+            invalidStudentException.AddData(
+                key: nameof(Student.Id),
+                values: "Id is required");
+
+            var expectedStudentValidationException =
+                new StudentValidationException(invalidStudentException);
+
+            //when
+            ValueTask<Student> retrieveStudentByIdTask =
+                this.studentService.RetrieveStudentByIdAsync(invalidStudentId);
+
+            StudentValidationException actualStudentValidationException =
+                await Assert.ThrowsAsync<StudentValidationException>(retrieveStudentByIdTask.AsTask);
+
+            //then
+            actualStudentValidationException.Should().BeEquivalentTo(
+                expectedStudentValidationException);
+
+            this.loggingBrokerMock.Verify(broker=>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentValidationException))),Times.Once);
+
+            this.storageBrokerMock.Verify(broker=>
+                 broker.SelectStudentByIdAsync(It.IsAny<Guid>()),Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
