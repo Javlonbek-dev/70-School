d7586d96fe788254d8826ba6c2c3064ec733fde9
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-03-18 13:29:01 +0300

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfStudentIsNotFoundAndLogItAsync -> FAIL

---
+++ 70-School.Web1/Models/Students/Exceptions/NotFoundStudentException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace _70_School.Web1.Models.Students.Exceptions
+{
+    public class NotFoundStudentException:Xeption
+    {
+        public NotFoundStudentException(Guid studentId)
+            :base(message: $"Couldn't find postReport with id:{studentId}.")
+        { }
+    }
+}
