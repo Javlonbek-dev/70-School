bbe45bc76a2f54d3afd817ac0099ec5cda2152c3
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-03-18 13:52:16 +0300

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ 70-School.Tests.Unit/Foundations/Students/StudnetsServiceTests.Exception.RetrieveById.cs
@@ -0,0 +1,48 @@
+﻿using _70_School.Web1.Models.Students;
+using _70_School.Web1.Models.Students.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace _70_School.Tests.Unit.Foundations.Students
+{
+    public partial class StudnetsServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            Guid someStudentId = Guid.NewGuid();
+            SqlException sqlException = CreateRandomException();
+
+            var failedStudentStorageException =
+                new FailedStudentStorageException(sqlException);
+
+            var expectedStudentDependencyException =
+                new StudentDependencyException(failedStudentStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectStudentByIdAsync(someStudentId))
+                    .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<Student> retrieveStudentByIdTask =
+                this.studentService.RetrieveStudentByIdAsync(someStudentId);
+
+            StudentDependencyException actualStudentDependencyException =
+                await Assert.ThrowsAsync<StudentDependencyException>(retrieveStudentByIdTask.AsTask);
+
+            //then
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectStudentByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedStudentDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
